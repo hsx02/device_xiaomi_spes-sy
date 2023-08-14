c0a853dbc6b4dff7a5020db2850c94066198db68
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2023-08-14 11:43:22 +0530

    Update and rename blaze_spes.mk to lineage_spes.mk

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := blaze_spes
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -23,10 +23,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
+RISING_CHIPSET := "snapdragon680™"
+RISING_MAINTAINER := "hitesoni"
+TARGET_CORE_GMS_EXTRAS := false
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_ENABLE_BLUR := false
+TARGET_USE_PIXEL_FINGERPRINT := true
 
-# Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := sayann70
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
